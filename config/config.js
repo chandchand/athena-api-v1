c2d6eae744fc8de6c0120faa03a9023e00436dc0
chandchand <candrapurnama440@gmail.com>
2023-12-08 23:13:12 +0700

    config

--- config/config.js
+++ config/config.js
@@ -1,22 +1,43 @@
 require('dotenv').config();
 
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     environment: 'development', 
+//     production: process.env.DATABASE_URL
+//   },
+//   // production: {
+//   //   // Konfigurasi produksi (jika diperlukan)
+//   //   username: process.env.DB_PROD_USERNAME,
+//   //   password: process.env.DB_PROD_PASSWORD,
+//   //   database: process.env.DB_PROD_NAME,
+//   //   host: process.env.DB_PROD_HOST,
+//   //   dialect: 'postgres',
+//   //   environment: 'production', // Tambahkan properti environment
+//   // },
+// };
+
 module.exports = {
+
   development: {
+ 
     username: process.env.DB_USERNAME,
+ 
     password: process.env.DB_PASSWORD,
+ 
     database: process.env.DB_NAME,
+ 
     host: process.env.DB_HOST,
+ 
     dialect: 'postgres',
-    environment: 'development', 
-    production: process.env.DATABASE_URL
+ 
+    environment: 'development',
+ 
   },
-  // production: {
-  //   // Konfigurasi produksi (jika diperlukan)
-  //   username: process.env.DB_PROD_USERNAME,
-  //   password: process.env.DB_PROD_PASSWORD,
-  //   database: process.env.DB_PROD_NAME,
-  //   host: process.env.DB_PROD_HOST,
-  //   dialect: 'postgres',
-  //   environment: 'production', // Tambahkan properti environment
-  // },
+ 
+  production: process.env.DATABASE_URL,
 };
