33a73d74600b342a09425aaaa4f64fb48c9fb232
chandchand <candrapurnama440@gmail.com>
2023-12-19 16:50:37 +0700

    fix chat

--- models/chat/chatRoom.js
+++ models/chat/chatRoom.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose');
 
 const chatSchema = new mongoose.Schema({
   senderId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', // Referensi ke model User
+    type: String,// Referensi ke model User
     required: true,
   },
   receiverId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', // Referensi ke model User
+    type: String, // Referensi ke model User
     required: true,
   },
   message: {
