9f7854559f065bcb412665b7b78f0fafe63e3b62
chandchand <candrapurnama440@gmail.com>
2023-12-21 00:17:24 +0700

    socket anjgg emgg

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-// app.js
-const cors = require('cors')
+const cors = require('cors');
 const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const http = require('http');
+const socketIO = require('socket.io');
+const { setupSocket } = require('./utils/socketIo');
 const db = require('./models');
 const connectDB = require('./config/mongoDB');
 const ErrorHandler = require('./utils/errorHandlers');
 const error = require('./middlewares/errorMiddleware');
 const morgan = require('morgan');
-const http = require('http');
-const socketIO = require('socket.io');
-const { setupSocket } = require('./utils/socketIo');
+const ChatRoom = require('./models/chat/chatRoom'); // Import model MongoDB
 
 dotenv.config();
 
 const app = express();
-
 const server = http.createServer(app);
 const io = socketIO(server, {
     cors: {
@@ -24,11 +23,57 @@ const io = socketIO(server, {
      }
 });
 
-setupSocket(io);
+// Setup Socket.IO
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+
+  socket.on('sendMessage', async (data) => {
+    try {
+      const { senderId, receiverId, message } = data;
+
+      // Simpan pesan ke MongoDB
+      const chat = await ChatRoom.create({
+        senderId,
+        receiverId,
+        message,
+      });
+
+      // Kirim pesan ke penerima
+      io.to(receiverId).emit('receiveMessage', { chat });
+    } catch (err) {
+      console.error('Error sending message:', err);
+      // Handling error for socket.io event
+      socket.emit('errorMessage', { error: 'Failed to send message' });
+    }
+  });
+
+  socket.on('messageSeen', async (data) => {
+    try {
+      const { senderId, receiverId } = data;
+
+      // Update status pesan yang telah dilihat di MongoDB
+      await ChatRoom.updateMany(
+        { senderId: receiverId, receiverId: senderId },
+        { $set: { seen: true } }
+      );
+
+      // Kirim notifikasi ke pengirim bahwa pesan telah dilihat
+      io.to(senderId).emit('messageSeen', { receiverId });
+    } catch (err) {
+      console.error('Error updating message seen status:', err);
+      // Handling error for socket.io event
+      socket.emit('errorMessage', { error: 'Failed to update message seen status' });
+    }
+  });
+});
 
 const PORT = process.env.PORT || 8000;
-app.use(morgan('dev'));
 
+app.use(morgan('dev'));
 connectDB();
 
 db.sequelize.sync({ force: false, logging: (msg) => console.log(`[${db.sequelize.config.environment}] ${msg}`) })
@@ -41,8 +86,8 @@ db.sequelize.sync({ force: false, logging: (msg) => console.log(`[${db.sequelize
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cookieParser())
-app.use(cors())
+app.use(cookieParser());
+app.use(cors());
 
 const auth = require('./routes/authRoute');
 const user = require('./routes/userRoute');
@@ -50,18 +95,17 @@ const chat = require('./routes/chatRoute');
 const timeline = require('./routes/timelineRoute');
 
 app.get("/api/", (req,res) => {
-  res.send("Hello Word")
-})
+  res.send("Hello Word");
+});
 
 app.use('/api/auth', auth);
 app.use('/api/user', user);
 app.use('/api/chat', chat);
 app.use('/api/timeline', timeline);
-// app.use('/api/tasks', taskRoutes);
-app.use('*',( req, res, next)=> { return next( new ErrorHandler("PAGE NOT FOUND", 404)) }) 
+
+app.use('*',( req, res, next)=> { return next(new ErrorHandler("PAGE NOT FOUND", 404)); });
 app.use(error.errorMiddleware);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
-  // console.log(`Documentasi api dengan swagger: /api/api-docs`);
 });
