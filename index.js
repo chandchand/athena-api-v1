17998c1105a849d04795dc7a3720adde9116db66
chandchand <candrapurnama440@gmail.com>
2023-12-24 04:20:05 +0700

    FIX SOCKET.IO

--- index.js
+++ index.js
@@ -23,46 +23,36 @@ const io = socketIO(server, {
      }
 });
 
+const activeRooms = new Map();
+
 io.on('connection', (socket) => {
   console.log('User connected');
 
-  socket.on('joinRoom', async ({ roomId, userId }) => {
+  socket.on('joinOrCreateRoom', async ({ userId, otherUserId }) => {
     try {
-      // Lakukan validasi atau logika pembuatan ruang obrolan
-      let room = await RoomChat.findOne({ _id: roomId });
-
-      if (!room) {
-        // Jika ruang obrolan belum ada, buat ruang obrolan baru
-        room = new RoomChat({
-          _id: roomId,
-          participants: [{ user: userId }],
-        });
-        await room.save();
-      } else {
-        // Jika ruang obrolan sudah ada, periksa apakah pengguna sudah bergabung
-        const isUserAlreadyJoined = room.participants.some((participant) => participant.user === userId);
-        if (!isUserAlreadyJoined) {
-          room.participants.push({ user: userId });
-          await room.save();
-        }
-      }
+      // Cari atau buat ruang obrolan
+      const roomId = getOrCreateRoomId(userId, otherUserId);
 
       // Bergabung ke ruang obrolan
       socket.join(roomId);
       console.log(`User ${userId} joined room ${roomId}`);
+
+      // Kirim histori chat jika ada
+      const chatHistory = getChatHistory(roomId);
+      socket.emit('chatHistory', { roomId, chatHistory });
     } catch (error) {
-      console.error('Error joining room:', error);
+      console.error('Error joining or creating room:', error);
     }
   });
 
   socket.on('sendMessage', (data) => {
-    // Pastikan ruang obrolan sudah ada atau buat baru
-    RoomChat.findOneAndUpdate(
-      { _id: data.roomId, 'participants.user': data.sender },
-      { $setOnInsert: { _id: data.roomId, participants: [{ user: data.sender }] } },
-      { upsert: true, new: true }
-    )
+    // Pastikan ruang obrolan sudah ada
+    RoomChat.findById(data.roomId)
       .then((room) => {
+        if (!room) {
+          throw new Error(`Room not found: ${data.roomId}`);
+        }
+
         // Simpan pesan ke database
         const newMessage = new Message({
           roomId: data.roomId,
@@ -83,7 +73,7 @@ io.on('connection', (socket) => {
         io.to(data.roomId).emit('receiveMessage', { ...savedMessage.toObject(), seenBy: [seenByInfo] });
       })
       .catch((error) => {
-        console.error('Error creating or updating room:', error);
+        console.error('Error sending message:', error);
       });
   });
 
@@ -93,6 +83,23 @@ io.on('connection', (socket) => {
     // ... (tindakan saat user disconnect, misalnya update status online/offline)
   });
 });
+
+function getOrCreateRoomId(userId, otherUserId) {
+  // Sorting agar ID ruang obrolan tetap sama tidak peduli siapa yang pertama kali mengunjungi
+  const sortedUserIds = [userId, otherUserId].sort();
+  const roomId = sortedUserIds.join('_');
+
+  if (!activeRooms.has(roomId)) {
+    // Jika ruang obrolan belum ada, buat ruang obrolan baru
+    activeRooms.set(roomId, { participants: [userId, otherUserId], chatHistory: [] });
+  }
+
+  return roomId;
+}
+
+function getChatHistory(roomId) {
+  return activeRooms.get(roomId)?.chatHistory || [];
+}
 
 
 const PORT = process.env.PORT || 8000;
